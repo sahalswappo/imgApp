2342de25e3462a43d0776163772e50c90f846ec4
Sahal <mdsahal@ifca.com.my>
2015-09-18 18:01:34 +0800

    Upload

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
 var path = require('path');
 
-app.use(express.static(__dirname + 'static'));
-app.use('static', express.static('public'));
+
+var upload = multer({
+    dest: 'static/upload/'
+});
+
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.post('/api/upload', upload, function(req, res, next) {
+    res.send(req.files);
+});
+
+
 app.listen(3000);
