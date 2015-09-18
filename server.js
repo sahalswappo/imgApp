584244fbf4a98b14d10fb9ef31d6f05165393d93
caal <sahal.swappo@gmail.com>
2015-09-19 01:59:44 +0800

    create icon

--- server.js
+++ server.js
@@ -17,26 +17,33 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 
 });
-// var im = require('imagemagick');
-gm = require('gm').subClass({imageMagick: true});
+
+var gm = require('gm').subClass({
+    imageMagick: true
+});
 
 app.post('/api/upload', upload, function(req, res, next) {
     if (req.files) {
+        var imageUploaded = req.files.image.path;
         //get image size
-        var sizeOf = require('image-size');
-var dimensions = sizeOf(req.files.image.path);
-console.log(dimensions.width, dimensions.height);
-        gm(req.files.image.path)
-        .size(function (err, size) {
-          if (!err)
-            console.log(size.width > size.height ? 'wider' : 'taller than you');
-        });
-        // im.identify(req.files.image.path, function(err, size) {
-        //     if (err) throw err;
-        //     imgSize = size.height;
-        // console.log(imgSize)
-        // });
-        // console.log(imgSize)
+        var is = require('image-size');
+        var size = is(imageUploaded);
+        var needResize = 0;
+        //get how many thumbnail needed
+        if (size.height > 128 && size.width > 128)
+            needResize = 3;
+        else if (size.height > 64 && size.width > 64)
+            needResize = 2;
+        else if (size.height > 32 && size.width > 32)
+            needResize = 1;
+        //create the thumbnail
+        for (var i = 1; i <= needResize; i++) {
+            var newSize = Math.pow(2, (i+4));
+            var thumbnailpath = 'app/upload/thumb' + newSize + "/" + req.files.image.name;
+            gm(imageUploaded)
+                .resize(newSize, newSize, '^')
+                .write(thumbnailpath);
+        };
         res.send(req.files);
     }
 });
