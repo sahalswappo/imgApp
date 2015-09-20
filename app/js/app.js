adc2c1ede8d6226b45dad9b2b4098c4b52be60a3
caal <sahal.swappo@gmail.com>
2015-09-21 00:46:46 +0800

    Complete paging

--- app/js/app.js
+++ app/js/app.js
@@ -1,46 +1,92 @@
 //chirpApp.js
-var app = angular.module('imgApp', []);
+var app = angular.module('imgApp', ['ngRoute']);
 
-app.controller('mainController', function($scope, $http, fileUpload) {
-    // alert('work');
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/', {
+            templateUrl: 'view/home.html',
+            controller: 'UploadCtrl'
+        }).
+        when('/gallery', {
+            templateUrl: 'view/gallery.html',
+            controller: 'GalleryCtrl'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }
+])
+
+.controller('UploadCtrl', function($scope, $http, Database) {
     $scope.submit = function() {
         var image = $scope.imgFile;
         var uploadUrl = '/api/upload';
-        fileUpload.uploadFileToUrl(image, uploadUrl).then(function(data) {
-            console.log(data.data);
-            if(data.data.image) {                
-               $scope.imgUrl = 'upload/' + data.data.image.name;
+        Database.uploadFileToUrl(image, uploadUrl).then(function(data) {
+            if (data.data.image) {
+                $scope.imgUrl = 'upload/' + data.data.image.name;
             }
         });
     }
 })
 
-.directive("imgUp", [function() {
-    return {
-        scope: {
-            imgUp: "="
-        },
-        link: function(scope, element, attributes) {
-            element.bind("change", function(changeEvent) {
-                scope.$apply(function() {
-                    scope.imgUp = changeEvent.target.files[0];
+.controller('GalleryCtrl', function($scope, $http, Database) {
+    $scope.currentPage = 0;    
+    $scope.page = function(page) {
+        Database.getImageInfo(page).then(function(data) {
+            $scope.galleries = data.data;
+        })
+        $scope.currentPage = page;
+    }
+
+    $scope.page();
+    Database.TotalPage().then(function(data) {
+        $scope.totalPage = Math.floor(data.data / 2);
+    });
+    
+})
+
+.directive("imgUp", [
+
+    function() {
+        return {
+            scope: {
+                imgUp: "="
+            },
+            link: function(scope, element, attributes) {
+                element.bind("change", function(changeEvent) {
+                    scope.$apply(function() {
+                        scope.imgUp = changeEvent.target.files[0];
+                    });
                 });
-            });
+            }
         }
     }
-}])
+])
 
-.service('fileUpload', ['$http', function($http) {
-    this.uploadFileToUrl = function(image, uploadUrl) {
-        var fd = new FormData();
-        fd.append('image', image);
-        var promise = $http.post(uploadUrl, fd, {
-                transformRequest: angular.identity,
-                headers: {
-                    'Content-Type': undefined
-                }
-            })
-            .success(function(data) {});
-        return promise;
+.service('Database', ['$http',
+    function($http) {
+        this.uploadFileToUrl = function(image, uploadUrl) {
+            var fd = new FormData();
+            fd.append('image', image);
+            var promise = $http.post(uploadUrl, fd, {
+                    transformRequest: angular.identity,
+                    headers: {
+                        'Content-Type': undefined
+                    }
+                })
+                .success(function(data) {});
+            return promise;
+        }
+        this.getImageInfo = function(page) {
+            var promise = $http.get('/gallery?page=' + page)
+                .success(function(data) {});
+            return promise;
+        }
+        this.TotalPage = function(page) {
+            var promise = $http.get('/totalpage')
+                .success(function(data) {});
+            return promise;
+        }
     }
-}]);
+])
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var multer = require('multer');
 var path = require('path');
-
+var Datastore = require('nedb'),
+    db = new Datastore({
+        filename: 'db/imagedb.db',
+        autoload: true
+    });
+var itemPerPage = 2;
 
 var upload = multer({
     dest: 'app/upload/',
@@ -15,18 +20,35 @@ app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-
 });
+
+app.get('/gallery', function(req, res) {
+    var skip = 0;
+    if (req.query.page) {
+        skip = req.query.page * itemPerPage;
+    }
+    db.find({}).skip(skip).limit(itemPerPage).exec(function(err, docs) {
+        res.send(docs)
+        res.end();
+    });
+});
+
+app.get('/totalpage', function(req, res) {
+    db.count({}, function(err, count) {
+        res.send(count.toString())
+    });
+})
 
 var gm = require('gm').subClass({
     imageMagick: true
 });
+var is = require('image-size');
 
 app.post('/api/upload', upload, function(req, res, next) {
     if (req.files) {
         var imageUploaded = req.files.image.path;
+        var imageName = req.files.image.name;
         //get image size
-        var is = require('image-size');
         var size = is(imageUploaded);
         var needResize = 0;
         //get how many thumbnail needed
@@ -38,19 +60,28 @@ app.post('/api/upload', upload, function(req, res, next) {
             needResize = 1;
         //create the thumbnail
         for (var i = 1; i <= needResize; i++) {
-            var newSize = Math.pow(2, (i+4));
-            var thumbnailpath = 'app/upload/thumb' + newSize + "/" + req.files.image.name;
+            var newSize = Math.pow(2, (i + 4));
+            var thumbnailpath = 'app/upload/thumb' + newSize + "/" + imageName;
             gm(imageUploaded)
                 .resize(newSize, newSize, '^')
                 .write(thumbnailpath, function(err) {
-                    if (!err) {
-                        console.log(' hooray! ');
-                    } 
+                    if (err) console.log(err)
                 });
         };
+        //save upload image inside db
+        var saveImage = {
+            image: imageName,
+            uploadDate: new Date()
+        };
+        InserData(saveImage);
         res.send(req.files);
+        res.end();
     }
 });
 
+//Insert data into database
+function InserData(data) {
+    db.insert(data, function(err, newDoc) {});
+}
 
 app.listen(3000);
